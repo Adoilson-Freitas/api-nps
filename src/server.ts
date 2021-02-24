636f59704296f72179314989b4368e52e7620b16
Adoilson Freitas <adoilsonfreitas@gmail.com>
2021-02-23 23:28:03 -0300

    criando controllers

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import "reflect-metadata";
 import express from 'express';
 import "./database";
+import { router } from "./routes";
 
 const app = express()
 
-app.get("/", (req, res) => {
-  return res.json({message: "Oi"})
-})
+app.use(express.json());
+app.use(router);
 
-app.post("/", (req, res) => {
-  return res.json({message: "OK"})
-})
 app.listen(3333) 
